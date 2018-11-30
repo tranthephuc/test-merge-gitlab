6c54cb1def84a945d4a21143cf1d084f8fc535c0
Phuc Tran <phuc.tran@jeppesen.com>
2018-11-30 00:20:36 -0700

    FPT say hello!

--- src/main/java/com/phuc/study/Hello.java
+++ src/main/java/com/phuc/study/Hello.java
@@ -3,5 +3,7 @@ package com.phuc.study;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("This is an origional project!");
+		
+		System.out.println("We are FPT team");
 	}
 }

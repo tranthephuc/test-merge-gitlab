d26771e678a3de9e3a63b5e82fe2a3600841f14d
Phuc Tran <phuc.tran@jeppesen.com>
2018-11-30 00:30:17 -0700

    Update master 1 2 3

--- src/main/java/com/phuc/study/Hello.java
+++ src/main/java/com/phuc/study/Hello.java
@@ -3,5 +3,8 @@ package com.phuc.study;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("This is an origional project!");
+		System.out.println("Master is 1");
+		System.out.println("Master is 2");
+		System.out.println("Master is 3");
 	}
 }
